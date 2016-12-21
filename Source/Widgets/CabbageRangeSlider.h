0cd8802a907ba5264765da9aca43083b3ee66c08
rorywalsh <rorywalsh@ear.ie>
2016-12-21 17:52:44 +0000

    added range slider

--- Source/Widgets/CabbageRangeSlider.h
+++ Source/Widgets/CabbageRangeSlider.h
@@ -23,98 +23,20 @@
 #include "../CabbageCommonHeaders.h"
 #include "CabbageWidgetBase.h"
 
-//============================================================
-// homegrown version of JUCE's two value, slider with dragable range
-// see below for CabbageRangeSlider class...
-//============================================================
-class RangeSlider	:	public Component
+
+class CabbageRangeSlider  : public Slider, public ValueTree::Listener, public CabbageWidgetBase
 {
-    String name, tooltipText;
-    StringArray channels;
-    bool shouldDisplayPopup, isVertical;
-    String colour, text;
-    Colour trackerColour;
-    double min, trackerThickness, initVal, minVal, maxVal, max, incr, skew;
-    float thumbIncr;
-    int thumbWidth, thumbHeight, rangeDistance, currentThumb, index;
-    float width, height;
+	void mouseDown (const MouseEvent& event) override;
+    void mouseDrag (const MouseEvent& event) override;
+    void valueChanged() override;
 
-
-public:
-    Rectangle<int> sliderThumbBounds;
-
-    class SliderThumb : public Component
-    {
-        Colour colour;
-		bool isVertical;
-
-    public:
-        SliderThumb(bool certical);
-        ~SliderThumb(){}
-        void resized(){}
-        void paint(Graphics& g);
-        void setColour(Colour _colour)
-        {
-            colour = _colour;
-        };
-    };
-
-
-    RangeSlider(ValueTree wData);
-    ~RangeSlider(){}
-
-    enum SliderType
-    {
-        left,
-        right,
-        top,
-        bottom
-    };
-
-
-    void setIndex(int _index)
-    {
-        index = _index;
-    }
-
-    void setValue(float val1, float val2);
-    void resized();
-    void mouseDown(const MouseEvent& event);
-    void mouseUp(const MouseEvent& event);
-    void mouseEnter(const MouseEvent& event);
-    void mouseDrag(const MouseEvent& event);
-    void mouseMove(const MouseEvent& event);
-    void horizontalDrag(const MouseEvent& event);
-    void verticalDrag(const MouseEvent& event);
-    void paint(Graphics& g);
-    int getSliderPosition(SliderType type);
-    void showPopup();
-    double getSkewedValue(double proportion);
-    int getSkewedPosition(double proportion);
-    void sendValuesToCsound(double val1, double val2, int thumb);
-
-    Rectangle<int> getThumbOuterBounds(int type);
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RangeSlider);
-
-private:
-    SliderThumb sliderThumb;
-};
-
-//========================================================================================
-class CabbageRangeSlider : public Slider, public ValueTree::Listener, public CabbageWidgetBase
-{
-    String name, text, textColour;
-    RangeSlider slider;
-    Label textLabel;
-    bool isVertical;
-    float initVal1, initVal2, min, max;
+    bool mouseDragBetweenThumbs;
+    float xMinAtThumbDown;
+    float xMaxAtThumbDown;
 	
 public:
-
-    CabbageRangeSlider(ValueTree wData);
-    ~CabbageRangeSlider() {};
-	
-	void resized();
+    CabbageRangeSlider(ValueTree wData);    
+    ~CabbageRangeSlider(){};
 
     //ValueTree::Listener virtual methods....
     void valueTreePropertyChanged (ValueTree& valueTree, const Identifier&);
@@ -123,11 +45,13 @@ public:
     void valueTreeChildOrderChanged (ValueTree&, int, int) override {}
     void valueTreeParentChanged (ValueTree&) override {};
 
-    ValueTree widgetData;
+	LookAndFeel_V2 lookAndFeel;
 
+	ValueTree widgetData;
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CabbageRangeSlider);
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CabbageRangeSlider);
 };
+
 
 
 #endif  // CABBAGERANGESLIDER_H_INCLUDED
