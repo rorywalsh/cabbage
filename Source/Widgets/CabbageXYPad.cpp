ee708da9559e2b94193d7944c89dda6f226a2622
rorywalsh <rorywalsh@ear.ie>
2016-12-21 07:44:23 +0000

    updating xy pad

--- Source/Widgets/CabbageXYPad.cpp
+++ Source/Widgets/CabbageXYPad.cpp
@@ -15,6 +15,8 @@
   License along with Csound; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
   02111-1307 USA
+
+  Based on the XYPad by Damien Rennick 
 */
 
 #include "CabbageXYPad.h"
--- Source/Widgets/CabbageXYPad.h
+++ Source/Widgets/CabbageXYPad.h
@@ -15,6 +15,9 @@
   License along with Csound; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
   02111-1307 USA
+
+  Based on the XYPad by Damien Rennick
+   
 */
 
 #ifndef CABBAGEXYPAD_H_INCLUDED
