82e09e2ffb05dec389ee99a55832b69d3ff09a87
rorywalsh <rorywalsh@ear.ie>
2017-02-15 22:57:06 +0000

    fixed filelogger issue

--- Source/Audio/Plugins/CsoundPluginProcessor.cpp
+++ Source/Audio/Plugins/CsoundPluginProcessor.cpp
@@ -107,7 +107,7 @@ CsoundPluginProcessor::CsoundPluginProcessor (File csdFile, bool debugMode)
 CsoundPluginProcessor::~CsoundPluginProcessor()
 {
     CabbageUtilities::debug ("Plugin destructor");
-	fileLogger = nullptr;
+	Logger::setCurrentLogger (nullptr);
 
     if (csound)
     {
