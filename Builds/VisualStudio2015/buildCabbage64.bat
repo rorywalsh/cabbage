bc1e8f269475bd4dbda4169e0c468682451674b3
rorywalsh <rorywalsh@ear.ie>
2017-05-12 00:36:49 +0100

    updating build script on Windows

--- Builds/VisualStudio2015/buildCabbage64.bat
+++ Builds/VisualStudio2015/buildCabbage64.bat
@@ -4,15 +4,17 @@ ECHO "Then modify the jucer files in the Cabbage root directory to ensure all pa
 C:\\Users\\rory\\sourcecode\\JUCE\\extras\\Projucer\\Builds\\VisualStudio2015\\x64\\Debug\\Projucer.exe --resave ../../CabbageIDE.jucer
 msbuild Cabbage.vcxproj /p:Configuration=Release /property:Platform=x64
 
+cp ".\x64\Release\Cabbage.exe" ".\x64\Cabbage.exe"
+
 C:\\Users\\rory\\sourcecode\\JUCE\\extras\\Projucer\\Builds\\VisualStudio2015\\x64\\Debug\\Projucer.exe --resave ../../CabbagePlugin.jucer
 msbuild CabbagePlugin.vcxproj /p:Configuration=Release /property:Platform=x64 
 
-xcopy ".\x64\Release\CabbagePlugin.dll" ".\x64\Release\CabbagePluginEffect.dll\" /y
+cp ".\x64\Release\CabbagePlugin.dll" ".\x64\CabbagePluginEffect.dll"
 
 C:\\Users\\rory\\sourcecode\\JUCE\\extras\\Projucer\\Builds\\VisualStudio2015\\x64\\Debug\\Projucer.exe --resave ../../CabbagePlugin.jucer
 msbuild CabbagePlugin.vcxproj /p:Configuration=Release /property:Platform=x64 /p:Cabbage_Plugin_Synth=1
 
-xcopy ".\x64\Release\CabbagePlugin.dll" ".\x64\Release\CabbagePluginSynth.dll\"  /y
+cp ".\x64\Release\CabbagePlugin.dll" ".\x64\CabbagePluginSynth.dll"
 
 C:\\Users\\rory\\sourcecode\\JUCE\\extras\\Projucer\\Builds\\VisualStudio2015\\x64\\Debug\\Projucer.exe --resave ../../CabbageIDE.jucer
 
