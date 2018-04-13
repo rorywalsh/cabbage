a21f0280e30faeee054ac957d26ccfd9909b0e20
rorywalsh <rorywalsh@ear.ie>
2018-04-13 14:46:20 +0100

    pushing release for xypad issue

--- Source/CabbageCommonHeaders.h
+++ Source/CabbageCommonHeaders.h
@@ -24,7 +24,8 @@
 #pragma warning(disable: 4100)
 #pragma warning(disable: 4305)
 
-#define CABBAGE_VERSION "Cabbage(64bit) v2.0.02"#include "./Settings/CabbageSettings.h"
+#define CABBAGE_VERSION "Cabbage(64bit) v2.0.02"
+#include "./Settings/CabbageSettings.h"
 #include "LookAndFeel/CabbageIDELookAndFeel.h"
 #include "LookAndFeel/CabbageLookAndFeel2.h"
 #include "Widgets/CabbageWidgetData.h"
