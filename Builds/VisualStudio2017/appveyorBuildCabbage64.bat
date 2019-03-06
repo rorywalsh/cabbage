08da0eabb24628a2274ec23f535cd7685cd2db53
rorywalsh <rorywalsh@ear.ie>
2019-03-06 20:24:20 +0000

    updating appveyor build

--- Builds/VisualStudio2017/appveyorBuildCabbage64.bat
+++ Builds/VisualStudio2017/appveyorBuildCabbage64.bat
@@ -1,4 +1,3 @@
-
 REM "Build Script for Cabbage on Windows. 
 REM "Please edit this file to point to the location of your Projucer application"
 REM "And change the variable MYPATH to the directory of projucer.exe"
@@ -16,23 +15,22 @@ REM "BUILDING THE MAIN APP"
  copy ".\x64\Release\App\Cabbage.exe" ".\x64\Cabbage.exe"
 
  
-REM REM "BUILDING EFFECT PLUGIN"
-REM  %PROJUCER% --resave ../../CabbagePlugin.jucer
-REM  msbuild CabbagePlugin.sln /p:Configuration=Release /property:Platform=x64 
-REM  copy ".\x64\Release\VST\CabbagePlugin.dll" ".\x64\CabbagePluginEffect.dll"
+REM "BUILDING EFFECT PLUGIN"
+ %PROJUCER% --resave ../../CabbagePlugin.jucer
+ msbuild CabbagePlugin.sln /p:Configuration=Release /property:Platform=x64 
+ copy ".\x64\Release\VST\CabbagePlugin.dll" ".\x64\CabbagePluginEffect.dll"
 
 
-REM REM "BUILDING VST PLUGIN"
-REM  %PROJUCER% --resave ../../CabbagePluginSynth.jucer
-REM  msbuild CabbagePlugin.sln /p:Configuration=Release /property:Platform=x64
+REM "BUILDING VST PLUGIN"
+ %PROJUCER% --resave ../../CabbagePluginSynth.jucer
+ msbuild CabbagePlugin.sln /p:Configuration=Release /property:Platform=x64
 
-REM  copy ".\x64\Release\VST\CabbagePlugin.dll" ".\x64\CabbagePluginSynth.dll"
+ copy ".\x64\Release\VST\CabbagePlugin.dll" ".\x64\CabbagePluginSynth.dll"
 
-REM REM "BUILDING CABBAGE LITE"
-REM  %PROJUCER% --resave ../../CabbageLite.jucer
-REM   msbuild CabbageLite.sln /p:Configuration=Release /property:Platform=x64
-REM  copy ".\x64\Release\App\CabbageLite.exe" ".\x64\CabbageLite.exe"
+REM "BUILDING CABBAGE LITE"
+ %PROJUCER% --resave ../../CabbageLite.jucer
+  msbuild CabbageLite.sln /p:Configuration=Release /property:Platform=x64
+ copy ".\x64\Release\App\CabbageLite.exe" ".\x64\CabbageLite.exe"
 
 
-REM REM %MYPATH%Projucer.exe --resave ../../CabbageIDE.jucer
-
+REM %MYPATH%Projucer.exe --resave ../../CabbageIDE.jucer
