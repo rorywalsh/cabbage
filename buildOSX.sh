392bbf71af9551cf6446891168ee5b784ab0e6df
rorywalsh <rorywalsh@ear.ie>
2019-03-10 14:48:57 +0000

    updating OSX scirpt

--- buildOSX.sh
+++ buildOSX.sh
@@ -97,4 +97,4 @@ cd /Users/vsts/agent/2.148.0/work/1/s/Builds/MacOSX/
 
 packagesbuild InstallerAzure.pkgproj
 ls build
-cp /Users/vsts/agent/2.148.0/work/1/s/Builds/MacOSX/Cabbage.pkg /Users/vsts/agent/2.148.0/work/1/s/Cabbage.pkg
+cp /Users/vsts/agent/2.148.0/work/1/s/Builds/MacOSX/build/Release/Cabbage.pkg /Users/vsts/agent/2.148.0/work/1/s/Cabbage.pkg
