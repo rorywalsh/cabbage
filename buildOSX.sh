2601d785026f78a7eb6b752b0a4473a35778dd8a
rorywalsh <rorywalsh@ear.ie>
2019-03-20 09:35:54 +0000

    updating azure

--- buildOSX.sh
+++ buildOSX.sh
@@ -1,6 +1,12 @@
-cd $(Agent.BuildDirectory)
-ls
-pwd
+echo "Hello World"
+echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
+echo "AGENT_WORKFOLDER contents:"
+ls -1 $AGENT_WORKFOLDER
+echo "AGENT_BUILDDIRECTORY is $AGENT_BUILDDIRECTORY"
+echo "AGENT_BUILDDIRECTORY contents:"
+ls -1 $AGENT_BUILDDIRECTORY
+echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
+echo "Over and out."
 
 # pwd
 # curl -L -o Csound6.12.1-MacOS_universal.dmg 'https://github.com/csound/csound/releases/download/6.12.2/Csound6.12.1-MacOS_universal.dmg'
