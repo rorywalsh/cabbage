cfa6354b409a1d8465a8151c60355e04b59bcfae
rorywalsh <rorywalsh@ear.ie>
2019-09-13 11:06:53 +0100

    updating bump script

--- bump.sh
+++ bump.sh
@@ -1,7 +1,37 @@
+#!/bin/bash  
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+echo "Linux"
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
 ../JUCE/extras/Projucer/Builds/MacOSX/build/Debug/Projucer.app/Contents/MacOS/Projucer --bump-version CabbageIDE.jucer
 ../JUCE/extras/Projucer/Builds/MacOSX/build/Debug/Projucer.app/Contents/MacOS/Projucer --bump-version CabbagePlugin.jucer
 ../JUCE/extras/Projucer/Builds/MacOSX/build/Debug/Projucer.app/Contents/MacOS/Projucer --bump-version CabbagePluginSynth.jucer
 ../JUCE/extras/Projucer/Builds/MacOSX/build/Debug/Projucer.app/Contents/MacOS/Projucer --bump-version CabbageLite.jucer
 ../JUCE/extras/Projucer/Builds/MacOSX/build/Debug/Projucer.app/Contents/MacOS/Projucer --bump-version CabbagePluginMIDIEffect.jucer
+
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+echo "cygwin"
+
+elif [[ "$OSTYPE" == "msys" ]]; then
+../JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe --bump-version CabbageIDE.jucer
+../JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe --bump-version CabbagePlugin.jucer
+../JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe --bump-version CabbagePluginSynth.jucer
+../JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe --bump-version CabbageLite.jucer
+../JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe --bump-version CabbagePluginMIDIEffect.jucer
+
+elif [[ "$OSTYPE" == "win32" ]]; then
+../JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe --bump-version CabbageIDE.jucer
+../JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe --bump-version CabbagePlugin.jucer
+../JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe --bump-version CabbagePluginSynth.jucer
+../JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe --bump-version CabbageLite.jucer
+../JUCE/extras/Projucer/Builds/VisualStudio2019/x64/Debug/App/Projucer.exe --bump-version CabbagePluginMIDIEffect.jucer
+
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+echo "Unlikely"
+else
+echo "What bloody OS are you using dude?!?!"
+fi
+
 git add *.jucer
 git add Source
