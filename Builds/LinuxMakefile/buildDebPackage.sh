16cc1990731c26ec3b5bae21e9c1340f596b8f9d
rorywalsh <rorywalsh@ear.ie>
2020-01-22 09:50:44 +0000

    updating

--- Builds/LinuxMakefile/buildDebPackage.sh
+++ Builds/LinuxMakefile/buildDebPackage.sh
@@ -8,10 +8,10 @@ mkdir -p Cabbage/usr/share/doc/cabbage/CabbageManual
 mkdir -p Cabbage/usr/share/icons/hicolor/512x512/apps
 
 
-cp /usr/bin/CabbagePluginEffect.so Cabbage/usr/bin/CabbagePluginEffect.so
-cp /usr/bin/CabbagePluginSynth.so Cabbage/usr/bin/CabbagePluginSynth.so
-cp /usr/bin/CabbageLite Cabbage/usr/bin/Cabbage
-cp /usr/bin/testCsoundFile Cabbage/usr/bin/testCsoundFile
+cp CabbagePluginEffect.so Cabbage/usr/bin/CabbagePluginEffect.so
+cp CabbagePluginSynth.so Cabbage/usr/bin/CabbagePluginSynth.so
+cp Cabbage Cabbage/usr/bin/Cabbage
+# cp /usr/bin/testCsoundFile Cabbage/usr/bin/testCsoundFile
 
 cp ./../opcodes.txt Cabbage/usr/bin/opcodes.txt
 cp ./../../Images/cabbage.png Cabbage/usr/share/icons/hicolor/512x512/apps/cabbage.png
@@ -19,8 +19,8 @@ cp ./../../Images/cabbage.png Cabbage/usr/share/icons/hicolor/512x512/apps/cabba
 cp -rf  /usr/share/doc/cabbage/Examples Cabbage/usr/share/doc/cabbage/Examples
 cp -rf  ../../CabbageManual Cabbage/usr/share/doc/cabbage/CabbageManual
 
-cp Cabbage.desktop ~/.local/share/applications/
-cp CabbageLite.desktop ~/.local/share/applications/
+# cp Cabbage.desktop ~/.local/share/applications/
+# cp CabbageLite.desktop ~/.local/share/applications/
 
 cp control Cabbage/DEBIAN/control
 
