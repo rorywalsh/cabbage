0a1a585c7b2a535d3edcf032ad81823078df1def
roryywalsh <rorywalsh@ear.ie>
2020-01-30 17:05:19 +0000

    updating build script to include LV2 builds

--- Builds/LinuxMakefile/build/intermediate/Release/BinaryData_ce4232d4.d
+++
@@ -1,2 +0,0 @@
-build/intermediate/Release/BinaryData_ce4232d4.o: \
- ../../JuceLibraryCode/BinaryData.cpp
--- Builds/LinuxMakefile/build/intermediate/Release/CabbageBinaryData_812f3f1.d
+++
@@ -1,3 +0,0 @@
-build/intermediate/Release/CabbageBinaryData_812f3f1.o: \
- ../../Source/BinaryData/CabbageBinaryData.cpp \
- ../../Source/BinaryData/CabbageBinaryData.h
