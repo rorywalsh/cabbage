b07e81776ac19055705bf94776f411ee1b012b4c
rorywalsh <rorywalsh@ear.ie>
2020-07-15 16:03:22 +0100

    updating export options

--- Source/Utilities/CabbageExportPlugin.cpp
+++ Source/Utilities/CabbageExportPlugin.cpp
@@ -1,12 +1,12 @@
 /*
-  ==============================================================================
-
-    CabbageExportPlugin.cpp
-    Created: 27 Nov 2017 2:37:25pm
-    Author:  rory
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ CabbageExportPlugin.cpp
+ Created: 27 Nov 2017 2:37:25pm
+ Author:  rory
+ 
+ ==============================================================================
+ */
 
 #include "CabbageExportPlugin.h"
 
@@ -28,8 +28,8 @@ void PluginExporter::exportPlugin (String type, File csdFile, String pluginId, S
 #else
         String currentApplicationDirectory = thisFile.getParentDirectory().getFullPathName();
 #endif
-
-
+        
+        
         if (SystemStats::getOperatingSystemType() == SystemStats::OperatingSystemType::Linux)
         {
             fileExtension = "so";
@@ -44,19 +44,19 @@ void PluginExporter::exportPlugin (String type, File csdFile, String pluginId, S
                 fileExtension = "vst";
             else
                 fileExtension = "component";
-
+            
             currentApplicationDirectory = thisFile.getFullPathName() + "/Contents";
         }
         else
         {
             if(type == "Standalone")
                 fileExtension = "exe";
-			else if(type.contains("VST3"))
-				fileExtension = "vst3";
-			else
-	            fileExtension = "dll";
+            else if(type.contains("VST3"))
+                fileExtension = "vst3";
+            else
+                fileExtension = "dll";
         }
-
+        
         const String pluginDesc = String(JucePlugin_Desc);
         
         if (type == "VSTi" || type == "AUi" || type == "VST3i")
@@ -86,15 +86,15 @@ void PluginExporter::exportPlugin (String type, File csdFile, String pluginId, S
         {
             pluginFilename = currentApplicationDirectory + "/CabbagePlugin." + fileExtension;
         }
-
+        
         File VSTData (pluginFilename);
-
+        
         if (!VSTData.exists())
         {
             CabbageUtilities::showMessage("Error", pluginFilename + " cannot be found? It should be in the Cabbage root folder", &lookAndFeel);
             return;
         }
-
+        
         //if batch converting plugins
         if(File(destination).exists())
         {
@@ -102,26 +102,26 @@ void PluginExporter::exportPlugin (String type, File csdFile, String pluginId, S
             writePluginFileToDisk(newFile, csdFile, VSTData, fileExtension, pluginId, type,
                                   encrypt);
         }
-		else if(promptForFilename == false)
-		{
-			const String newFile = csdFile.getParentDirectory().getFullPathName()+"/"+csdFile.getFileNameWithoutExtension();
-			writePluginFileToDisk(newFile, csdFile, VSTData, fileExtension, pluginId, type,
-				encrypt);
-
-		}
+        else if(promptForFilename == false)
+        {
+            const String newFile = csdFile.getParentDirectory().getFullPathName()+"/"+csdFile.getFileNameWithoutExtension();
+            writePluginFileToDisk(newFile, csdFile, VSTData, fileExtension, pluginId, type,
+                                  encrypt);
+            
+        }
         else
         {
-
+            
             FileChooser fc("Save file as..", csdFile.getParentDirectory().getFullPathName(), "*." + fileExtension,
                            CabbageUtilities::shouldUseNativeBrowser());
-
+            
             if (fc.browseForFileToSave(false))
             {
                 if (fc.getResult().existsAsFile())
                 {
                     const int result = CabbageUtilities::showYesNoMessage("Do you wish to overwrite\nexiting file?",
                                                                           &lookAndFeel);
-
+                    
                     if (result == 1)
                     {
                         File oldFile(fc.getResult().getFullPathName());
@@ -133,7 +133,7 @@ void PluginExporter::exportPlugin (String type, File csdFile, String pluginId, S
                 else
                     writePluginFileToDisk(fc.getResult(), csdFile, VSTData, fileExtension, pluginId, type,
                                           encrypt);
-
+                
             }
         }
     }
@@ -142,9 +142,9 @@ void PluginExporter::exportPlugin (String type, File csdFile, String pluginId, S
 
 void PluginExporter::writePluginFileToDisk (File fc, File csdFile, File VSTData, String fileExtension, String pluginId, String type,  bool encrypt)
 {
-
+    
     File exportedPlugin (fc.withFileExtension (fileExtension).getFullPathName());
-
+    
     //vcv rack export is the same on all platforms..
     if(type=="VCVRack")
     {
@@ -157,11 +157,11 @@ void PluginExporter::writePluginFileToDisk (File fc, File csdFile, File VSTData,
         oldFile.deleteFile();
         //bundle all auxiliary files
         addFilesToPluginBundle(csdFile, exportedPlugin);
-
+        
         File jsonFile(exportedPlugin.getFullPathName()+"/plugin.json");
         StringArray jsonLines;
         jsonLines.addLines(jsonFile.loadFileAsString());
-
+        
         for ( int i = 0 ; i < jsonLines.size() ; i++)
         {
             //replace slugs and name with plugin name
@@ -171,14 +171,14 @@ void PluginExporter::writePluginFileToDisk (File fc, File csdFile, File VSTData,
                 jsonLines.getReference(i) = "\"name\": \""+fc.getFileNameWithoutExtension()+"\",";
             if(jsonLines[i].contains("\"brand\": \"CabbageRack\","))
                 jsonLines.getReference(i) = "\"brand\": \""+fc.getFileNameWithoutExtension()+"\",";
-
+            
         }
-
+        
         jsonFile.replaceWithText(jsonLines.joinIntoString("\n"));
-
+        
         return;
     }
-
+    
     //plugin files on OSX are bundles, so we need to recursively delete all files in bundle
     if((SystemStats::getOperatingSystemType() & SystemStats::MacOSX) != 0)
     {
@@ -195,21 +195,21 @@ void PluginExporter::writePluginFileToDisk (File fc, File csdFile, File VSTData,
         CabbageUtilities::showMessage ("Error", "Exporting: " + csdFile.getFullPathName() + ", Can't copy plugin to this location. It currently be in use, or you may be trying to install to a system folder you don't have permission to write in. Please try exporting to a different location.", &lookAndFeel);
         return;
     }
-
-
+    
+    
     File exportedCsdFile;
-
+    
     
     if ((SystemStats::getOperatingSystemType() & SystemStats::MacOSX) != 0)
     {
-        
+        const String pluginDesc = String(JucePlugin_Desc);
         if(fileExtension.containsIgnoreCase("component"))
-            exportedCsdFile = exportedPlugin.getFullPathName() + String ("/Contents/CabbagePlugin.csd");
+            exportedCsdFile = exportedPlugin.getFullPathName() + String ("/Contents/"+pluginDesc+".csd");
         else if(fileExtension.containsIgnoreCase("vst"))
             exportedCsdFile = exportedPlugin.getFullPathName() + String ("/Contents/") + fc.getFileNameWithoutExtension() + String (".csd");
         else if(fileExtension.containsIgnoreCase("app"))
             exportedCsdFile = exportedPlugin.getFullPathName() + String ("/Contents/") + fc.getFileNameWithoutExtension() + String (".csd");
-
+        
         if(fileExtension.contains("dylib"))
         {
             exportedCsdFile = fc.withFileExtension (".csd").getFullPathName();
@@ -223,25 +223,25 @@ void PluginExporter::writePluginFileToDisk (File fc, File csdFile, File VSTData,
             }
             else
                 exportedCsdFile.replaceWithText (csdFile.loadFileAsString());
-
-            File bin (exportedPlugin.getFullPathName() + String ("/Contents/MacOS/CabbagePlugin"));
+            
+            File bin (exportedPlugin.getFullPathName() + String ("/Contents/MacOS/"+pluginDesc));
             //if(bin.exists())showMessage("binary exists");
-
+            
             File pl (exportedPlugin.getFullPathName() + String ("/Contents/Info.plist"));
             String newPList = pl.loadFileAsString();
             
             if(fileExtension.containsIgnoreCase("vst") || fileExtension.containsIgnoreCase("app"))
             {
                 File pluginBinary (exportedPlugin.getFullPathName() + String ("/Contents/MacOS/") + fc.getFileNameWithoutExtension());
-
+                
                 if (bin.moveFileTo (pluginBinary) == false)
                     CabbageUtilities::showMessage ("Error", "Could not copy library binary file. Make sure the two Cabbage .vst files are located in the Cabbage.app folder", &lookAndFeel);
-
+                
                 setUniquePluginId (pluginBinary, exportedCsdFile, pluginId);
-               
-                newPList = newPList.replace ("CabbagePlugin", fc.getFileNameWithoutExtension());
+                
+                newPList = newPList.replace (pluginDesc, fc.getFileNameWithoutExtension());
             }
-
+            
             
             String manu(JucePlugin_Manufacturer);
             const String pluginName = "<string>" +manu + ": " + fc.getFileNameWithoutExtension() + "</string>";
@@ -250,6 +250,8 @@ void PluginExporter::writePluginFileToDisk (File fc, File csdFile, File VSTData,
             //be sure to remove CabbageAudio from plugin plist..
             const String toReplace2 = "<string>CabbageAudio: CabbageEffectNam<string>";
             newPList = newPList.replace (toReplace2, pluginName);
+            newPList = newPList.replace ("<string>CabbagePlugin</string>", "<string>" +pluginDesc + "</string>");
+            
 #endif
             newPList = newPList.replace (toReplace, pluginName);
             if(pluginId.isEmpty())
@@ -265,7 +267,7 @@ void PluginExporter::writePluginFileToDisk (File fc, File csdFile, File VSTData,
         }
         //bundle all auxiliary files
         addFilesToPluginBundle(csdFile, exportedCsdFile);
-
+        
     }
     else
     {
@@ -274,12 +276,12 @@ void PluginExporter::writePluginFileToDisk (File fc, File csdFile, File VSTData,
         {
             exportedCsdFile.replaceWithText (encodeString(csdFile));
         }
-
+        
         else
             exportedCsdFile.replaceWithText (csdFile.loadFileAsString());
-
+        
         setUniquePluginId (exportedPlugin, exportedCsdFile, pluginId);
-
+        
         //bundle all auxiliary files
         addFilesToPluginBundle(csdFile, exportedPlugin);
     }
@@ -292,46 +294,46 @@ void PluginExporter::writePluginFileToDisk (File fc, File csdFile, File VSTData,
     
 #ifdef CabbagePro //bundle and relink Csound
     //removing this for now to see if I can relink manually...
-//    if ((SystemStats::getOperatingSystemType() & SystemStats::MacOSX) != 0 && encrypt)
-//    {
-//        File csoundFramework(File::getSpecialLocation (File::currentApplicationFile).getFullPathName()+"/Contents/CsoundLib64.framework");
-//        if(!csoundFramework.exists())
-//            jassertfalse;
-//
-//        if(!csoundFramework.copyFileTo(File(exportedPlugin.getFullPathName()+"/Contents/Resources/CsoundLib64.framework")))
-//            jassertfalse;
-//
-//        ChildProcess childProc;
-//        String pluginBinaryPath;
-//
-//        if(fileExtension.containsIgnoreCase("vst"))
-//            pluginBinaryPath = exportedPlugin.getFullPathName() + String ("/Contents/MacOS/") + fc.getFileNameWithoutExtension();
-//        else
-//            pluginBinaryPath = exportedPlugin.getFullPathName() + String ("/Contents/MacOS/CabbagePlugin");
-//
-//
-//
-//        if(!childProc.start("otool -L "+pluginBinaryPath))
-//        {
-//            CabbageUtilities::showMessage("You will need Xcode's CLI developer tools in order to include Csound in your plugin bundle. Please install them and try again.", &lookAndFeel);
-//            return;
-//        }
-//
-//
-//        StringArray lines;
-//        lines.addLines(childProc.readAllProcessOutput());
-//        const String csoundPath = lines[1].substring(0, lines[1].indexOf("("));
-//        const String install_name_tool = "install_name_tool -change " + csoundPath + "@loader_path/../Resources/CsoundLib64.framework/CsoundLib64 /" + pluginBinaryPath;
-//
-//
-//        childProc.start(install_name_tool);
-//        if(childProc.readAllProcessOutput().length()>1)
-//            CabbageUtilities::showMessage("There was a problem relinking the Csound binaries: " + childProc.readAllProcessOutput() + " Please contact Cabbage support and quote the following error message:", &lookAndFeel);
-//
-//
-//
-//
-//    }
+    //    if ((SystemStats::getOperatingSystemType() & SystemStats::MacOSX) != 0 && encrypt)
+    //    {
+    //        File csoundFramework(File::getSpecialLocation (File::currentApplicationFile).getFullPathName()+"/Contents/CsoundLib64.framework");
+    //        if(!csoundFramework.exists())
+    //            jassertfalse;
+    //
+    //        if(!csoundFramework.copyFileTo(File(exportedPlugin.getFullPathName()+"/Contents/Resources/CsoundLib64.framework")))
+    //            jassertfalse;
+    //
+    //        ChildProcess childProc;
+    //        String pluginBinaryPath;
+    //
+    //        if(fileExtension.containsIgnoreCase("vst"))
+    //            pluginBinaryPath = exportedPlugin.getFullPathName() + String ("/Contents/MacOS/") + fc.getFileNameWithoutExtension();
+    //        else
+    //            pluginBinaryPath = exportedPlugin.getFullPathName() + String ("/Contents/MacOS/CabbagePlugin");
+    //
+    //
+    //
+    //        if(!childProc.start("otool -L "+pluginBinaryPath))
+    //        {
+    //            CabbageUtilities::showMessage("You will need Xcode's CLI developer tools in order to include Csound in your plugin bundle. Please install them and try again.", &lookAndFeel);
+    //            return;
+    //        }
+    //
+    //
+    //        StringArray lines;
+    //        lines.addLines(childProc.readAllProcessOutput());
+    //        const String csoundPath = lines[1].substring(0, lines[1].indexOf("("));
+    //        const String install_name_tool = "install_name_tool -change " + csoundPath + "@loader_path/../Resources/CsoundLib64.framework/CsoundLib64 /" + pluginBinaryPath;
+    //
+    //
+    //        childProc.start(install_name_tool);
+    //        if(childProc.readAllProcessOutput().length()>1)
+    //            CabbageUtilities::showMessage("There was a problem relinking the Csound binaries: " + childProc.readAllProcessOutput() + " Please contact Cabbage support and quote the following error message:", &lookAndFeel);
+    //
+    //
+    //
+    //
+    //    }
 #endif
     
     
@@ -345,7 +347,7 @@ int PluginExporter::setUniquePluginId (File binFile, File csdFile, String plugin
     size_t file_size;
     //both mac and Windows encode the plugin IDs differently, so we run through a loop of both possibilities..
     const char* pluginIDToReplace[] = { "RORY", "YROR" };
-
+    
     long loc;
     std::fstream mFile (binFile.getFullPathName().toUTF8(), ios_base::in | ios_base::out | ios_base::binary);
     
@@ -451,15 +453,15 @@ void PluginExporter::addFilesToPluginBundle (File csdFile, File exportDir)
     bool invalidFilename = false;
     StringArray csdArray;
     csdArray.addLines (csdFile.loadFileAsString());
-
+    
     if(csdArray.indexOf("<CabbageIncludes>"))
     {
         for (int i = csdArray.indexOf("<CabbageIncludes>") + 1; i < csdArray.indexOf("</CabbageIncludes>"); i++)
         {
-
+            
             File includeFile(csdFile.getParentDirectory().getFullPathName() + "/" + csdArray[i]);
             File newFile(exportDir.getParentDirectory().getFullPathName() + "/" + csdArray[i]);
-
+            
             if (includeFile.exists())
             {
                 includeFile.copyFileTo(newFile);
@@ -469,21 +471,21 @@ void PluginExporter::addFilesToPluginBundle (File csdFile, File exportDir)
                 invalidFiles.add(includeFile.getFullPathName());
             }
         }
-
+        
         if (invalidFiles.size() > 0)
             CabbageUtilities::showMessage(
-                    "Cabbage could not bundle the following files\n" + invalidFiles.joinIntoString("\n") +
-                    "\nPlease make sure they are located in the same folder as your .csd file.", &lookAndFeel);
+                                          "Cabbage could not bundle the following files\n" + invalidFiles.joinIntoString("\n") +
+                                          "\nPlease make sure they are located in the same folder as your .csd file.", &lookAndFeel);
     }
-
+    
     StringArray linesFromCsd;
     linesFromCsd.addLines(csdFile.loadFileAsString());
-
+    
     for( auto lineOfCode : linesFromCsd)
     {
         if(lineOfCode.contains("</Cabbage>"))
             return;
-
+        
         ValueTree temp ("temp");
         CabbageWidgetData::setWidgetState (temp, lineOfCode, 0);
         if (CabbageWidgetData::getStringProp(temp, CabbageIdentifierIds::type) == CabbageWidgetTypes::form)
@@ -494,7 +496,7 @@ void PluginExporter::addFilesToPluginBundle (File csdFile, File exportDir)
                 {
                     const File bundleFile = csdFile.getParentDirectory().getChildFile (bundleFiles[i].toString());
                     File newFile(exportDir.getParentDirectory().getFullPathName() + "/" + bundleFiles[i].toString());
-
+                    
                     if (bundleFile.existsAsFile())
                         bundleFile.copyFileTo(newFile);
                     else if(bundleFile.exists())
@@ -506,12 +508,12 @@ void PluginExporter::addFilesToPluginBundle (File csdFile, File exportDir)
                 }
         }
     }
-
+    
     if (invalidFiles.size() > 0)
         CabbageUtilities::showMessage(
-                "Cabbage could not bundle the following files\n" + invalidFiles.joinIntoString("\n") +
-                "\nPlease make sure they are located in the same folder as your .csd file.", &lookAndFeel);
-
+                                      "Cabbage could not bundle the following files\n" + invalidFiles.joinIntoString("\n") +
+                                      "\nPlease make sure they are located in the same folder as your .csd file.", &lookAndFeel);
+    
 }
 
 
@@ -523,11 +525,11 @@ long PluginExporter::cabbageFindPluginId (unsigned char* buf, size_t len, const 
     size_t imax = len - slen - 1;
     long ret = -1;
     int match;
-
+    
     for (i = 0; i < imax; i++)
     {
         match = 1;
-
+        
         for (j = 0; j < slen; j++)
         {
             if (buf[i + j] != s[j])
@@ -536,14 +538,14 @@ long PluginExporter::cabbageFindPluginId (unsigned char* buf, size_t len, const 
                 break;
             }
         }
-
+        
         if (match)
         {
             ret = i;
             break;
         }
     }
-
+    
     //return position of plugin ID
     return ret;
 }
