e4893094f6d6d6f0b69acf5ba43c7230df514d68
rorywalsh <rorywalsh@ear.ie>
2020-09-09 09:54:43 +0100

    updating Csound version for OSX pacakge

--- buildOSX.sh
+++ buildOSX.sh
@@ -14,13 +14,13 @@ ls -1 $BUILD_ARTIFACTSTAGINGDIRECTORY
 
   
 pwd
-curl -L -o csound6.13.0-MacOS_x86_64.dmg 'https://github.com/csound/csound/releases/download/6.13.0/Csound6.13-MacOS_x86_64.dmg'
+curl -L -o csound6.15.0-MacOS_x86_64.dmg 'https://github.com/csound/csound/releases/download/6.15.0/csound-MacOS_x86_64-6.15.0.dmg'
 ls
-hdiutil attach csound6.13.0-MacOS_x86_64.dmg
-cp -R /Volumes/Csound6.13/ Csound
-hdiutil detach /Volumes/Csound6.13/
+hdiutil attach csound6.15.0-MacOS_x86_64.dmg
+cp -R /Volumes/Csound6.15.0/ Csound
+hdiutil detach /Volumes/Csound6.15.0/
 cd Csound
-sudo installer -pkg csound6.13-MacOS_x86_64.pkg -target /
+sudo installer -pkg csound-MacOS_x86_64-6.15.0.pkg -target /
 sudo install_name_tool -id /Library/Frameworks/CsoundLib64.framework/CsoundLib64  /Library/Frameworks/CsoundLib64.framework/CsoundLib64
 
 cd $AGENT_BUILDDIRECTORY
