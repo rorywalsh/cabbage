c041e155290f34e93a4a65db2881088c3c79c4c1
rorywalsh <rorywalsh@ear.ie>
2021-03-24 20:39:45 +0000

    fixing another button issue in VST3 automation

--- Source/Widgets/CabbageButton.cpp
+++ Source/Widgets/CabbageButton.cpp
@@ -97,11 +97,12 @@ void CabbageButton::valueTreePropertyChanged(ValueTree& valueTree, const Identif
 
 	if (prop == CabbageIdentifierIds::value)
 	{
-		CabbageUtilities::debug(CabbageWidgetData::getStringProp(valueTree, CabbageIdentifierIds::name));
-		CabbageUtilities::debug(CabbageWidgetData::getNumProp(valueTree, CabbageIdentifierIds::value));
-        const int newValue = CabbageWidgetData::getNumProp(valueTree, CabbageIdentifierIds::value);
-        setValue(newValue > 0.9 ? 1 : 0);
-		setToggleState(newValue == 0 ? false : true, dontSendNotification);
+		//CabbageUtilities::debug(CabbageWidgetData::getStringProp(valueTree, CabbageIdentifierIds::name));
+		//CabbageUtilities::debug(CabbageWidgetData::getNumProp(valueTree, CabbageIdentifierIds::value));
+        const int newValue = CabbageWidgetData::getNumProp(valueTree, CabbageIdentifierIds::value)  > 0.9 ? 1 : 0;
+        setValue(newValue);
+		//bool shouldEnable = (newValue == 0 ? false : true);
+        setToggleState(newValue == 0 ? false : true, dontSendNotification);
 		setButtonText(getTextArray()[getValue()]);
 	}
 	else
--- Source/Widgets/CabbageCheckbox.cpp
+++ Source/Widgets/CabbageCheckbox.cpp
@@ -74,7 +74,7 @@ void CabbageCheckbox::valueTreePropertyChanged (ValueTree& valueTree, const Iden
 
     if (prop == CabbageIdentifierIds::value)
     {
-        const int newValue = CabbageWidgetData::getNumProp(valueTree, CabbageIdentifierIds::value)  > 0.99 ? 1 : 0;
+        const int newValue = CabbageWidgetData::getNumProp(valueTree, CabbageIdentifierIds::value)  > 0.9 ? 1 : 0;
         bool state = newValue == 1 ? true : false;
         setToggleState (state, sendNotification);
     }
