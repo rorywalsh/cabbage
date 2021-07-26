f9752c0d349cc61e1b4e7cb244c9271d0d5efe76
rorywalsh <rorywalsh@ear.ie>
2021-07-26 19:54:05 +0100

    fixing crash with cabbageGetReservedChannel

--- Source/Opcodes/CabbageIdentifierOpcodes.cpp
+++ Source/Opcodes/CabbageIdentifierOpcodes.cpp
@@ -1371,12 +1371,12 @@ int CabbageCopyFile::copyFiles()
 //====================================================================================================
 int GetCabbageReservedChannelString::getAttribute()
 {
-    String channel(inargs.str_data(0).data);
+
     if (csound->get_csound()->GetChannelPtr(csound->get_csound(), &value, inargs.str_data(0).data,
                                             CSOUND_STRING_CHANNEL | CSOUND_OUTPUT_CHANNEL) == CSOUND_SUCCESS)
     {
-        
-        outargs.str_data(0).data = csound->strdup(((STRINGDAT*)value)->data);
+        outargs.str_data(0).size = ((STRINGDAT*)value)->size;
+        outargs.str_data(0).data = ((STRINGDAT*)value)->data;
     }
     return OK;
 }
