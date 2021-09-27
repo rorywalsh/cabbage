1288a6deccdcb4d0a39183a987ff82aecc50615f
U-DESKTOP-56RNO12\rory <rorywalsh@ear.ie>
2021-09-27 18:36:23 +0100

    updating CMake stuff again

--- build.py
+++ build.py
@@ -20,9 +20,15 @@ if len(sys.argv) == 3:
 else:
     projects = ["Cabbage", "CabbagePluginEffect", "CabbagePluginSynth"]
 
-
 if not os.path.exists("JUCE"):
-    os.system('got clone https://github.com/juce-framework/JUCE.git')
+    os.system('git clone https://github.com/juce-framework/JUCE.git')
+    os.system('git apply ./patches/StandaloneWrapper.patch')
+    os.system('git apply ./patches/AUWrapper.patch')
+    os.system('git apply ./patches/UtilityWrapper.patch')
+    os.system('git apply ./patches/VST2Wrapper.patch')
+    os.system('git apply ./patches/VST3Wrapper.patch')
+
+
 
 if platform.system() == "Darwin" and os.path.exists("Cabbage.app"):
     os.system('rm -rf Cabbage.app')
@@ -66,7 +72,7 @@ for project in projects:
             os.system('cp -Rf Cabbage_artefacts/'+buildType+'/Cabbage.exe '+rootDir+'/CabbageBinaries/Cabbage.exe')
         elif project == "CabbagePluginEffect" or project == "CabbagePluginSynth":
             os.system('cp -Rf '+project+'_artefacts/'+buildType+'/VST/'+project+'.dll ' +rootDir+'/CabbageBinaries/'+project+'.dll')
-            os.system('cp -Rf '+project+'_artefacts/'+buildType+'/VST3/'+project+'.vst3 ' +rootDir+'/CabbageBinaries/'+project+'.vst3')
+            os.system('cp -Rf '+project+'_artefacts/'+buildType+'/VST3/'+project+'.vst3/Contents/x86_64-win/'+project+'.vst3 ' +rootDir+'/CabbageBinaries/'+project+'.vst3')
             if project == "CabbagePluginSynth":
                 os.system('cp -Rf '+project+'_artefacts/'+buildType+'/Standalone/'+project+'.exe ' +rootDir+'/CabbageBinaries/CabbagePlugin.exe')
 
