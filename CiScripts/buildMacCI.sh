cdff2eaf0ff0fb3beecf94e13b0205e403fa772c
rorywalsh <rorywalsh@ear.ie>
2021-09-28 14:17:16 +0100

    updating CI stuff

--- CiScripts/buildMacCI.sh
+++ CiScripts/buildMacCI.sh
@@ -23,21 +23,6 @@ cd Csound
 sudo installer -pkg csound-MacOS_x86_64-6.16.2.pkg -target /
 sudo install_name_tool -id /Library/Frameworks/CsoundLib64.framework/CsoundLib64  /Library/Frameworks/CsoundLib64.framework/CsoundLib64
 
-cd $AGENT_BUILDDIRECTORY
-git clone https://github.com/juce-framework/JUCE.git
-cd JUCE
-git checkout 5.4.7
-git branch
-touch JUCE/
-cd $AGENT_BUILDDIRECTORY/JUCE
-cd $AGENT_BUILDDIRECTORY/JUCE/extras/Projucer/JuceLibraryCode
-sed -i '' "s/#define JUCER_ENABLE_GPL_MODE 1/#define JUCER_ENABLE_GPL_MODE 1/" AppConfig.h
-sed -i '' "s/#define JUCE_USE_DARK_SPLASH_SCREEN 1/#define JUCE_USE_DARK_SPLASH_SCREEN 0/" AppConfig.h
-cat AppConfig.h
-cd $AGENT_BUILDDIRECTORY/JUCE/extras/Projucer/Builds/MacOSX/
-xcodebuild -project Projucer.xcodeproj
-
-pwd
 cd ~
 pwd
 ls
@@ -55,94 +40,38 @@ echo "\n"
 
 cd $AGENT_BUILDDIRECTORY
 
-curl -L -o Packages.dmg 'http://s.sudre.free.fr/Software/files/Packages.dmg'
-hdiutil mount Packages.dmg
-sudo installer -pkg /Volumes/Packages\ 1.2.9/Install\ Packages.pkg -target /
-hdiutil detach /Volumes/Packages\ 1.2.9/
+python build.py --config=Release
 
-cd $SYSTEM_DEFAULTWORKINGDIRECTORY/Builds/MacOSX
-export PROJUCER=$AGENT_BUILDDIRECTORY/JUCE/extras/Projucer/Builds/MacOSX/build/Debug/Projucer.app/Contents/MacOS/Projucer
-
-#building sinmple Csound CLI for testing files
-$PROJUCER --resave ../../CabbageCsoundCLI.jucer
-xcodebuild -project CabbageCsoundCLI.xcodeproj/ ARCHS="x86_64" ONLY_ACTIVE_ARCH=NO -configuration Release
-
-$PROJUCER --resave ../../CabbageIDE.jucer
-xcodebuild -project Cabbage.xcodeproj/ clean
-xcodebuild -project Cabbage.xcodeproj/ ARCHS="x86_64" ONLY_ACTIVE_ARCH=NO -configuration Release 
-cp -rf ../../Themes ./build/Release/Cabbage.app/Contents/Themes
-
-$PROJUCER --resave ../../CabbageLite.jucer
-xcodebuild -project CabbageLite.xcodeproj/ ARCHS="x86_64" ONLY_ACTIVE_ARCH=NO -configuration Release
-
-$PROJUCER --resave ../../CabbagePluginMIDIEffect.jucer
-xcodebuild -project CabbagePlugin.xcodeproj clean
-xcodebuild -project CabbagePlugin.xcodeproj/ ARCHS="x86_64" ONLY_ACTIVE_ARCH=NO -configuration Release GCC_PREPROCESSOR_DEFINITIONS="Cabbage_Plugin_Synth=1 Cabbage_MIDI_Effect=1 USE_DOUBLE=1 CSOUND6=1 MACOSX=1" 
-cp -rf ./build/Release/CabbagePlugin.component/ ./build/Release/Cabbage.app/Contents/CabbagePluginMIDIEffect.component
-cp -rf ./build/Release/CabbagePlugin.component/ ./build/Release/CabbageLite.app/Contents/CabbagePluginMIDIEffect.component
-
-$PROJUCER --resave ../../CabbagePluginSynth.jucer
-xcodebuild -project CabbagePlugin.xcodeproj clean
-xcodebuild -project CabbagePlugin.xcodeproj/ ARCHS="x86_64" ONLY_ACTIVE_ARCH=NO -configuration Release GCC_PREPROCESSOR_DEFINITIONS="Cabbage_Plugin_Synth=1 USE_DOUBLE=1 CSOUND6=1 MACOSX=1"
-cp -rf ./build/Release/CabbagePlugin.vst/ ./build/Release/Cabbage.app/Contents/CabbagePluginSynth.vst
-cp -rf ./build/Release/CabbagePlugin.vst/ ./build/Release/CabbageLite.app/Contents/CabbagePluginSynth.vst
-cp -rf ./build/Release/CabbagePlugin.vst3/ ./build/Release/Cabbage.app/Contents/CabbagePluginSynth.vst3
-cp -rf ./build/Release/CabbagePlugin.vst3/ ./build/Release/CabbageLite.app/Contents/CabbagePluginSynth.vst3
-cp -rf ./build/Release/CabbagePlugin.component/ ./build/Release/Cabbage.app/Contents/CabbagePluginSynth.component
-cp -rf ./build/Release/CabbagePlugin.component/ ./build/Release/CabbageLite.app/Contents/CabbagePluginSynth.component
-
-$PROJUCER --resave ../../CabbagePlugin.jucer 
-xcodebuild -project CabbagePlugin.xcodeproj clean
-xcodebuild -project CabbagePlugin.xcodeproj/ -configuration Release ARCHS="x86_64" ONLY_ACTIVE_ARCH=NO GCC_PREPROCESSOR_DEFINITIONS="MACOSX=1 USE_DOUBLE=1" 
-cp -rf ./build/Release/CabbagePlugin.vst/ ./build/Release/Cabbage.app/Contents/CabbagePluginEffect.vst
-cp -rf ./build/Release/CabbagePlugin.app/ ./build/Release/Cabbage.app/Contents/CabbagePlugin.app
-cp -rf ./build/Release/CabbagePlugin.vst/ ./build/Release/CabbageLite.app/Contents/CabbagePluginEffect.vst
-cp -rf ./build/Release/CabbagePlugin.vst3/ ./build/Release/Cabbage.app/Contents/CabbagePluginEffect.vst3
-cp -rf ./build/Release/CabbagePlugin.vst3/ ./build/Release/CabbageLite.app/Contents/CabbagePluginEffect.vst3
-cp -rf ./build/Release/CabbagePlugin.component/ ./build/Release/Cabbage.app/Contents/CabbagePluginEffect.component
-rm ./build/Release/Cabbage.app/Contents/CabbagePluginEffect.component/Contents/Resources/CabbagePlugin.rsrc
-cp -rf ./build/Release/CabbagePlugin.component/ ./build/Release/CabbageLite.app/Contents/CabbagePluginEffect.component
-cp -rf ../../Examples ./build/Release/Cabbage.app/Contents/Examples
-cp -rf ../../Examples ./build/Release/CabbageLite.app/Contents/Examples
-
-# $PROJUCER --resave ../../CabbageCsoundCLI.jucer
-# xcodebuild -project CabbageCsoundCLI.xcodeproj clean
-# xcodebuild -project CabbageCsoundCLI.xcodeproj -configuration Release
+# curl -L -o Packages.dmg 'http://s.sudre.free.fr/Software/files/Packages.dmg'
+# hdiutil mount Packages.dmg
+# sudo installer -pkg /Volumes/Packages\ 1.2.9/Install\ Packages.pkg -target /
+# hdiutil detach /Volumes/Packages\ 1.2.9/
 
 
 
-# cp ../../CsoundTestXcode/Build/Release/CsoundTest ./build/Release/Cabbage.app/Contents/MacOS/CsoundTest 
-cp -rf ./build/Release/CabbageCsoundCLI ./build/Release/Cabbage.app/Contents/MacOS/CabbageCsoundCLI 
-curl -L -o CabbageManual.zip 'http://cabbageaudio.com/beta/CabbageManual.zip'
-ls
-unzip -q -o "CabbageManual.zip" 
-ls
-cp -rf CabbageManual ././build/Release/Cabbage.app/Contents/CabbageManual
-cp -rf CabbageManual ././build/Release/CabbageLite.app/Contents/CabbageManual
+# cd $SYSTEM_DEFAULTWORKINGDIRECTORY
+# curl -L -o CabbageRack-1.0.0-mac.zip https://github.com/rorywalsh/CabbageRack/releases/download/v1.0/CabbageRack-1.0.0-mac.zip
+# unzip -q CabbageRack-1.0.0-mac.zip
+# ls
+# cp -R CabbageRack Cabbage.app/Contents/CabbageRack
+# ls Cabbage.app/Contents/
+# curl -L -o fmod_csound_fx.dylib 'https://github.com/rorywalsh/csoundfmod/releases/download/v2.0/fmod_csound_fx.dylib'
+# curl -L -o fmod_csound.dylib 'https://github.com/rorywalsh/csoundfmod/releases/download/v2.0/fmod_csound.dylib'
+# ls
+# cp fmod_csound.dylib Cabbage.app/Contents/fmod_csound.dylib
+# cp fmod_csound_fx.dylib Cabbage.app/Contents/fmod_csound_fx.dylib
+# ls Cabbage.app/Contents/
+# zip -r CabbageOSX.zip Cabbage.app CabbageLite.app 
 
-cd $SYSTEM_DEFAULTWORKINGDIRECTORY/Builds/MacOSX/build/Release/
-curl -L -o CabbageRack-1.0.0-mac.zip https://github.com/rorywalsh/CabbageRack/releases/download/v1.0/CabbageRack-1.0.0-mac.zip
-unzip -q CabbageRack-1.0.0-mac.zip
-ls
-cp -R CabbageRack Cabbage.app/Contents/CabbageRack
-ls Cabbage.app/Contents/
-curl -L -o fmod_csound_fx.dylib 'https://github.com/rorywalsh/csoundfmod/releases/download/v2.0/fmod_csound_fx.dylib'
-curl -L -o fmod_csound.dylib 'https://github.com/rorywalsh/csoundfmod/releases/download/v2.0/fmod_csound.dylib'
-ls
-cp fmod_csound.dylib Cabbage.app/Contents/fmod_csound.dylib
-cp fmod_csound_fx.dylib Cabbage.app/Contents/fmod_csound_fx.dylib
-ls Cabbage.app/Contents/
-zip -r CabbageOSX.zip Cabbage.app CabbageLite.app 
+# cd $SYSTEM_DEFAULTWORKINGDIRECTORY/Builds/MacOSX/
 
-cd $SYSTEM_DEFAULTWORKINGDIRECTORY/Builds/MacOSX/
+# sed -i "" -e "s|SOURCE_PATH|$SYSTEM_DEFAULTWORKINGDIRECTORY|" InstallerAzure.pkgproj
 
-sed -i "" -e "s|SOURCE_PATH|$SYSTEM_DEFAULTWORKINGDIRECTORY|" InstallerAzure.pkgproj
+# packagesbuild InstallerAzure.pkgproj
+# ls build  
+# pwd
 
-packagesbuild InstallerAzure.pkgproj
-ls build  
-pwd
+# VERSION="CabbageOSXInstaller"-$($PROJUCER --get-version ../../CabbageIDE.jucer)".pkg"
 
-VERSION="CabbageOSXInstaller"-$($PROJUCER --get-version ../../CabbageIDE.jucer)".pkg"
-
-cp $SYSTEM_DEFAULTWORKINGDIRECTORY/Builds/MacOSX/build/Cabbage.pkg $BUILD_ARTIFACTSTAGINGDIRECTORY/$VERSION
+# cp $SYSTEM_DEFAULTWORKINGDIRECTORY/Builds/MacOSX/build/Cabbage.pkg $BUILD_ARTIFACTSTAGINGDIRECTORY/$VERSION
 
