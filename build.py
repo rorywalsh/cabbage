ddd10f6a9bfe34ee40d1ab0db928f321760b75b7
U-DESKTOP-56RNO12\rory <rorywalsh@ear.ie>
2021-09-30 11:16:27 +0100

    @updating@

--- build.py
+++ build.py
@@ -112,7 +112,7 @@ if buildType is not "Local Debug":
             r = requests.get(url, allow_redirects=True)
             open('CabbageManual.zip', 'wb').write(r.content)       
             with zipfile.ZipFile("CabbageManual.zip", 'r') as zip_ref:
-                zip_ref.extractall((rootDir+'/CabbageInstall/CabbageManual')
+                zip_ref.extractall(rootDir+'/CabbageInstall/CabbageManual')
 
 
 if "Remote Release" in buildType:        
