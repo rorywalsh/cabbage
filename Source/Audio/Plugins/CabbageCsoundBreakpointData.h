5a6e42097a1f67304fc4d5b05bbd42775509ad66
rorywalsh <rorywalsh@ear.ie>
2021-10-29 11:34:25 +0100

    bumping - Version number:2.8.45

--- Source/Audio/Plugins/CabbageCsoundBreakpointData.h
+++ Source/Audio/Plugins/CabbageCsoundBreakpointData.h
@@ -16,7 +16,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
   02111-1307 USA
 */
-
+  
 #ifndef CABBAGECSOUNDBREAKPOINTDATA_H_INCLUDED  
 #define CABBAGECSOUNDBREAKPOINTDATA_H_INCLUDED
 
