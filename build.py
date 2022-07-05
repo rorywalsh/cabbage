ac2d01f9412eb38dca8923c7ec2df06cd8ff2dfd
rorywalsh <rorywalsh@ear.ie>
2022-07-05 10:41:42 +0100

    updating build script

--- build.py
+++ build.py
@@ -438,8 +438,9 @@ sys.stdout.write(GREEN)
 
 if not os.path.exists("JUCE/modules"):
     print('Cloning JUCE and applying patches....')
-    os.system('git clone --depth=1 https://github.com/juce-framework/JUCE')
+    os.system('git clone --depth=1 https://github.com/juce-framework/JUCE && cd JUCE')
     os.system('git checkout 6.1.6')
+    os.system('cd ..')
     os.system('git apply ./patches/StandaloneWrapper.patch')
     os.system('git apply ./patches/AUWrapper.patch')
     os.system('git apply ./patches/UtilityWrapper.patch')
