f376c1c6f94406c9c4fd6ec227b72f835674a6d8
rorywalsh <rorywalsh@ear.ie>
2022-09-06 10:50:06 +0100

    updated CLI app - Version number:2.8.136

--- Source/Converter/Main.cpp
+++ Source/Converter/Main.cpp
@@ -34,6 +34,10 @@ int main (int argc, char* argv[])
     String args;
     // Your code goes here!
     juce::ignoreUnused (argc, argv);
+    
+    std::cout << "Usage: CLIConverter --export-TYPE=\"name of csd file\" --destination=\"some absolute or relative dir\"\n";
+    std::cout << "If you leave out the destiontation, exports will be placed ino the same folder as the csd file\n\n";
+    
     for( int i = 0 ; i < argc ; i++)
     {
         args.append(argv[i], 1000);
@@ -42,16 +46,27 @@ int main (int argc, char* argv[])
     String file = args.substring(args.indexOf("--export-")+9);
     String type = file.substring(0, file.indexOf("="));
     file = file.substring(file.indexOf("=")+1, file.indexOf("--") != -1 ? file.indexOf("--") : 300);
-//    DBG(file);
+
     File csdFile(File::getSpecialLocation(File::SpecialLocationType::hostApplicationPath).getChildFile(file));
     String destination = args.substring(args.indexOf("--destination=")+14);
     File pluginDestination(File::getSpecialLocation(File::SpecialLocationType::hostApplicationPath).getChildFile(destination).getParentDirectory());
+
     
     if(!pluginDestination.exists())
     {
+        std::string c;
         pluginDestination = csdFile.getParentDirectory();
+        std::cout << "You did not specify an export location. As a result, your plugin will be created in the same directory as its csd file. This may lead to some csd files being overwritten during the exporting process. Do you wish to continue? Y/N";
+        
+        if(strcmp(argv[argc-1], "Y") != 0)
+        {
+            getline(std::cin, c);
+            if(c == "N" || c =="n")
+                return 0;
+        }
     }
-//    DBG(destination);
+
+    
 #if CabbagePro
     pluginExporter.exportPlugin (type, csdFile,  getPluginInfo (csdFile, "id"), pluginDestination.getFullPathName(), false, true);
 #else
