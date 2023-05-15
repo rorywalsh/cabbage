e72114085848cb63643593638b0435594391a2a4
rorywalsh <rorywalsh@ear.ie>
2023-05-15 11:15:07 +0100

    fixing issue with filenames and presets, removed quotation marks around string - Version number:2.9.49

--- Source/Audio/Plugins/CabbagePluginProcessor.cpp
+++ Source/Audio/Plugins/CabbagePluginProcessor.cpp
@@ -1187,10 +1187,10 @@ void CabbagePluginProcessor::restorePluginPreset(String presetName, String fileN
                                                                                CabbageIdentifierIds::ignorelastdir);
                     if(ignoreLastDir == 0)
                     {
-						const String absolutePath = (String(presetData.value().dump()).replaceCharacters("\\", "/"));
+						const String absolutePath = String(presetData.value().dump()).removeCharacters("\"");
                         /*const String absolutePath =
                         csdFile.getParentDirectory().getChildFile(String(presetData.value().dump()).replaceCharacters("\\", "/")).getFullPathName();*/
-                        CabbageWidgetData::setStringProp(valueTree, CabbageIdentifierIds::file, absolutePath.replaceCharacters("\\", "/"));
+                        CabbageWidgetData::setStringProp(valueTree, CabbageIdentifierIds::file, absolutePath.toUTF8().getAddress());
                     }
                 }
                 
