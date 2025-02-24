a08a2cd493d909ef75f833a6de1f24bd3bde9e64
rorywalsh <rorywalsh@ear.ie>
2025-02-24 18:45:59 +0000

    triggering new build - Version number:2.10.2

--- Source/Audio/Plugins/CabbagePluginProcessor.cpp
+++ Source/Audio/Plugins/CabbagePluginProcessor.cpp
@@ -10,7 +10,7 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU Lesser General Public License for more details.
-
+  
   You should have received a copy of the GNU General Public
   License along with Csound; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
